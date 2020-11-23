90315ed8a74ac6175150f830fff354b858fe627b
Nihal <nihal@mobiuso.com>
2020-11-23 22:41:31 +0530

    added file

---
+++ main2.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+
+
+for i in range(10000):
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    options.add_argument("--incognito")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    browser = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'),options=options)
+    browser.get('https://vote.globesoccer.com/vote/2020-century-player')
+    scroll_el= browser.find_element_by_xpath('/html/body/div/div/div[2]/form/div/label[10]')
+    messi_el = browser.find_element_by_xpath('/html/body/div/div/div[2]/form/div/label[14]')
+    messi_el.click();
+    browser.find_element_by_xpath('/html/body/div/div/div[2]/div/button').click();
+    print(i)
+    browser.quit()
